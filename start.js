66fd321ae237a889a96a3288906c0a0824e64450
Anastasiya-Shir <86313786+Anastasiya-Shir@users.noreply.github.com>
2021-09-15 06:53:37 +0300

    верстка страницы регистрации, переключение пока не работает

---
+++ start.js
@@ -0,0 +1,15 @@
+let form=document.getElementsByClassName("form");
+let div=document.getElementsByClassName("btn");
+let btn_send=document.getElementsByClassName("btn_send");
+let btnNewUser=document.getElementsByClassName("btn_new_user");
+let formNewUser=document.getElementsByClassName("form_new_user");
+
+let btnAddUser=document.getElementsByClassName("btn_add_user");
+
+btnNewUser.onclick=function(){
+    
+   
+    formNewUser.style.display="block";
+  
+
+};
